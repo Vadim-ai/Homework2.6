e8fe717a3096befb6dea56a8443909a0a0a7dc2b
SkyPro <vadim.phobia@gmail.com>
2022-06-28 00:40:37 +0300

    Выполнил ДЗ 2.6

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        List<Integer> myList = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+        List <String> myWords = new ArrayList<>(List.of("слон","клоп", "бегемот", "слон", "клоп", "пес"));
+
+        ServiceImpl service = new ServiceImpl();
+
+        System.out.println(service.getEvenNumbers(myList));
+        System.out.println(service.getOddNumbers(myList));
+        System.out.println(service.getUniqueWords(myWords));
+        System.out.println(service.getDuplicateWords(myWords));
     }
 }
